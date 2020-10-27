e163f5f027e078c0629f868e38a63f45407abd46
Antonov Igor <Gosha.8686@gmail.com>
2020-10-27 19:39:25 +0200

    jpg

--- src/components/Works/WorksCard/WorksCard.jsx
+++ src/components/Works/WorksCard/WorksCard.jsx
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {NavLink} from "react-router-dom";
 
 const WorksCard = (props) => {
+    debugger;
     return (
         <NavLink to={`/works/${props.path}`} className='works-card'
              style={{backgroundImage: `url(https://disconnect-ed.github.io/ruslan-dolgopol-portfolio/works/${props.img})`}}>
